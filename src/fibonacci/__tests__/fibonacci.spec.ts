96ed70edc189e71f2e996221ee2d6f2a0ade0e1a
Saff <saff.soobratty@gmail.com>
2018-09-01 13:48:07 +0100

    add mocha test

---
+++ src/fibonacci/__tests__/fibonacci.spec.ts
@@ -0,0 +1,26 @@
+import { performDurations } from '@detectiveQuack/utils';
+import { expect } from 'chai';
+import { join } from 'path';
+import iterative from '../iterative';
+import memoize from '../memoize';
+import recursive from '../recursive';
+
+describe('Fibonacci', () => {
+  it('should return 144 if 12 is passed in', () => {
+    expect(recursive(12)).to.equal(144);
+    expect(iterative(12)).to.equal(144);
+    expect(memoize(12)).to.equal(144);
+  });
+
+  it('should have memoize as the fastest and recursive as the slowest', () => {
+    const durations = performDurations(
+      join(__dirname, '..'),
+      '',
+      [20, 12, 32, 8],
+      false
+    );
+
+    expect(durations[0].algorithm).to.equal('memoize');
+    expect(durations[durations.length - 1].algorithm).to.equal('recursive');
+  });
+});
